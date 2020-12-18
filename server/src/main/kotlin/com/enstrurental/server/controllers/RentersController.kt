99ae205790452b8eead26c83e9f29cbf66b2f1d6
sedasimsek <sedasimsekk27@gmail.com>
2020-12-18 22:06:55 +0300

    RentersController güncellendi.

--- server/src/main/kotlin/com/enstrurental/server/controllers/RentersController.kt
+++ server/src/main/kotlin/com/enstrurental/server/controllers/RentersController.kt
@@ -1,4 +1,12 @@
 package com.enstrurental.server.controllers
 
-class RentersController {
+import com.enstrurental.server.entitites.RenterRepository
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("renters")
+class RentersController(@Autowired private val renterRepository: RenterController) {
+
 }
