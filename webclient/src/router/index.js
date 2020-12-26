d023f5546162bb8995d760ea0cab3faac0aaa70c
Fatih <fatihy@yahoo.com>
2020-12-26 16:29:09 +0300

    Vue 3'ten Vue 2'ye geçildi. Vuetify eklendi.

--- webclient/src/router/index.js
+++ webclient/src/router/index.js
@@ -1,5 +1,11 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+
+// View Imports
 import Home from '../views/Home.vue'
+import About from '../views/About.vue'
+
+Vue.use(VueRouter)
 
 const routes = [
   {
@@ -10,16 +16,13 @@ const routes = [
   {
     path: '/about',
     name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    component: About
   }
 ]
 
-const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
-  routes
+const router = new VueRouter({
+  routes,
+  mode: 'history'
 })
 
 export default router
