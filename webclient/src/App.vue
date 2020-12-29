b390e09dbb317212f3d99fa197fc9d1434cf3dda
Fatih <fatihy@yahoo.com>
2020-12-29 16:49:20 +0300

    Footer eklendi. Header'a fab eklendi.

--- webclient/src/App.vue
+++ webclient/src/App.vue
@@ -1,12 +1,13 @@
 <template>
   <v-app>
     <!-- Header -->
+    <!-- TODO: remove hide-on-scroll. Make custom function to hide app-bar -->
     <v-app-bar app
       color="primary"
       dark
       elevate-on-scroll
       hide-on-scroll
-      height="248px">
+      height="220px">
       <v-container>
         <!--LOGO-->
         <v-row no-gutters>
@@ -22,59 +23,90 @@
         </v-row>
         <!-- /LOGO -->
       </v-container>
-    <template v-slot:extension >
-      <v-toolbar color="primary"
-      outlined
-      elevation="0" dense>
-         <!-- Navbar -->
-        <v-row no-gutters class="mb-2">
-          <v-col class="d-flex justify-center">
-            <v-btn target="_blank" text class="mr-2">
-              <span class="mr-1">Ana Sayfa</span>
-              <v-icon>mdi-home</v-icon>
-            </v-btn>
-             <v-btn target="_blank" text class="mr-2">
-              <span class="mr-1">Nasıl Çalışır</span>
-              <v-icon>mdi-head-question-outline</v-icon>
-            </v-btn>
-            <v-btn target="_blank" text class="mr-2">
-              <span class="mr-1">Hakkımızda</span>
-              <v-icon>mdi-information-outline</v-icon>
-            </v-btn>
-          </v-col>
-        </v-row>
-      <!-- /Navbar -->
-      <!-- Fab button -->
-        <v-fab-transition>
-          <v-btn
-            v-show="!hidden"
-            color="warning"
-            fab
-            absolute
-            bottom
-            right
-          >
-            <v-icon>mdi-magnify</v-icon>
-          </v-btn>
-        </v-fab-transition>
-      <!-- /Fab button -->
-         </v-toolbar>
-    </template>
+      <!-- Toolbar -->
+      <template v-slot:extension >
+        <v-toolbar color="primary"
+          outlined
+          elevation="0" dense>
+          <!-- Navbar -->
+          <v-row no-gutters class="mb-2">
+            <v-col class="d-flex justify-center">
 
+              <v-btn target="_blank" text class="mr-2" v-for="(item, index) in menuItems" :key="index">
+                <span class="mr-1">{{item.title}}</span>
+                <v-icon>{{item.icon}}</v-icon>
+              </v-btn>
+
+            </v-col>
+          </v-row>
+          <!-- /Navbar -->
+          <!-- Fab button -->
+          <v-fab-transition>
+              <v-btn
+                id="search_bar"
+                color="accent"
+                fab
+                absolute
+                bottom
+                right
+              >
+                <v-icon>mdi-magnify</v-icon>
+              </v-btn>
+          </v-fab-transition>
+          <!-- /Fab button -->
+        </v-toolbar>
+      </template>
+      <!-- /Toolbar -->
     </v-app-bar>
     <!-- /Header -->
 
     <v-main id="main_view">
       <v-container fluid>
-        <router-view></router-view>
-
-        <!-- For test purposes -->
-      <div v-for="n in 20" :key="n">
-        <br>
-      </div>
+        <v-sheet
+          min-height="70vh"
+          rounded="xl"
+          class="mt-1" >
+          <router-view></router-view>
+        </v-sheet>
       </v-container>
     </v-main>
 
+  <!-- Footer -->
+   <v-footer
+      dark
+      padless >
+      <v-card
+      color="primary"
+        flat
+        tile
+        class="text-center flex"
+      >
+
+      <v-card-text id="footer-text" class="white--text">
+        {{ footer_text }}
+      </v-card-text>
+
+      <v-card-text>
+        <v-btn
+          v-for="icon in icons"
+          :key="icon"
+          class="mx-4 white--text"
+          icon
+        >
+          <v-icon size="24px">
+            {{ icon }}
+          </v-icon>
+        </v-btn>
+      </v-card-text>
+
+      <v-divider></v-divider>
+
+      <v-card-text class="white--text">
+        <p class="mute-text-footer">Copyright © {{ new Date().getFullYear() }} Enstrurent - All Rights Reserved.</p>
+      </v-card-text>
+    </v-card>
+  </v-footer>
+  <!-- /footer -->
   </v-app>
 </template>
 
@@ -84,19 +116,35 @@ export default {
   name: 'App',
 
   data: () => ({
+    footer_text: 'Eğer enstrümanlarınızı kiralatmak isteyen bir firmaysanız bize ulaşın!',
     menuItems: [
-      { title: 'Ana Sayfa', path: '/home', icon: 'home' },
+      { title: 'Ana Sayfa', path: '/home', icon: 'mdi-home' },
       { title: 'Hakkımızda', path: '/signup', icon: 'mdi-information-outline' },
       { title: 'Nasıl çalışır?', path: '/signin', icon: 'mdi-head-question-outline' }
+    ],
+    icons: [
+      'mdi-facebook',
+      'mdi-twitter',
+      'mdi-linkedin',
+      'mdi-instagram'
     ]
   })
 }
 </script>
-<style lang="scss" scoped>
-/* //To activate the purple background, uncomment. Remember <a> tags also purple.
+<style lang="scss">
+//To activate the purple background, uncomment. Remember <a> tags also purple.
 #main_view {
   background-color:#450d82;
   color: #fff;
   }
-   */
+  .mute-text-footer{
+    color: #cfccd8;
+    display: inline;
+    font-size: 14px;
+  }
+  #footer-text{
+    font-family: sans-serif;
+    font-size: 18px;
+  }
+
 </style>
