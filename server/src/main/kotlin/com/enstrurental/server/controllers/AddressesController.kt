8af5d4009352b69e09bedc91fcd07fdf0b68ae63
sedasimsek <sedasimsekk27@gmail.com>
2021-01-05 01:10:05 +0300

    Endpoint'ler eklendi, test edildi.

--- server/src/main/kotlin/com/enstrurental/server/controllers/AddressesController.kt
+++ server/src/main/kotlin/com/enstrurental/server/controllers/AddressesController.kt
@@ -2,7 +2,7 @@ package com.enstrurental.server.controllers
 
 import com.enstrurental.server.entitites.Addresses
 import com.enstrurental.server.entitites.AddressesRepository
-import com.enstrurental.server.entitites.Clients
+import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 import reactor.core.publisher.Flux
@@ -37,6 +37,89 @@ class AddressesController(val addressesRepository: AddressesRepository) {
         return addressesRepository.findAddressesByUid(uid)
     }
 
+    @PutMapping("/{id}/update_title/")
+    fun updateTitle(@PathVariable id: Int, @RequestBody address_updated: Addresses) : Mono<ResponseEntity<Addresses>>
+    {
+        return addressesRepository.findById(id)
+            .flatMap { address ->
+                address.title = address_updated.title
+                addressesRepository.save(address)
+            }
+            .map { address -> ResponseEntity.ok(address) }
+            .defaultIfEmpty(ResponseEntity(HttpStatus.NOT_FOUND))
+    }
 
+    @PutMapping("/{id}/update_city/")
+    fun updateCity(@PathVariable id: Int, @RequestBody address_updated: Addresses) : Mono<ResponseEntity<Addresses>>
+    {
+        return addressesRepository.findById(id)
+            .flatMap { address ->
+                address.city = address_updated.city
+                addressesRepository.save(address)
+            }
+            .map { address -> ResponseEntity.ok(address) }
+            .defaultIfEmpty(ResponseEntity(HttpStatus.NOT_FOUND))
+    }
+
+    @PutMapping("/{id}/update_state/")
+    fun updateState(@PathVariable id: Int, @RequestBody address_updated: Addresses) : Mono<ResponseEntity<Addresses>>
+    {
+        return addressesRepository.findById(id)
+            .flatMap { address ->
+                address.state = address_updated.state
+                addressesRepository.save(address)
+            }
+            .map { address -> ResponseEntity.ok(address) }
+            .defaultIfEmpty(ResponseEntity(HttpStatus.NOT_FOUND))
+    }
+
+    @PutMapping("/{id}/update_zipcode/")
+    fun updateZipcode(@PathVariable id: Int, @RequestBody address_updated: Addresses) :
+            Mono<ResponseEntity<Addresses>>
+    {
+        return addressesRepository.findById(id)
+            .flatMap { address ->
+                address.zipcode = address_updated.zipcode
+                addressesRepository.save(address)
+            }
+            .map { address -> ResponseEntity.ok(address) }
+            .defaultIfEmpty(ResponseEntity(HttpStatus.NOT_FOUND))
+    }
+
+    @PutMapping("/{id}/update_street/")
+    fun updateStreet(@PathVariable id: Int, @RequestBody address_updated: Addresses) : Mono<ResponseEntity<Addresses>>
+    {
+        return addressesRepository.findById(id)
+            .flatMap { address ->
+                address.street = address_updated.street
+                addressesRepository.save(address)
+            }
+            .map { address -> ResponseEntity.ok(address) }
+            .defaultIfEmpty(ResponseEntity(HttpStatus.NOT_FOUND))
+    }
+
+    @PutMapping("/{id}/update_building_no/")
+    fun updateBuildingNo(@PathVariable id: Int, @RequestBody address_updated: Addresses) : Mono<ResponseEntity<Addresses>>
+    {
+        return addressesRepository.findById(id)
+            .flatMap { address ->
+                address.building_no = address_updated.building_no
+                addressesRepository.save(address)
+            }
+            .map { address -> ResponseEntity.ok(address) }
+            .defaultIfEmpty(ResponseEntity(HttpStatus.NOT_FOUND))
+    }
+
+    @PutMapping("/{id}/update_description/")
+    fun updateDescription(@PathVariable id: Int, @RequestBody address_updated: Addresses) : Mono<ResponseEntity<Addresses>>
+    {
+        return addressesRepository.findById(id)
+            .flatMap { address ->
+                address.description = address_updated.description
+                addressesRepository.save(address)
+            }
+            .map { address -> ResponseEntity.ok(address) }
+            .defaultIfEmpty(ResponseEntity(HttpStatus.NOT_FOUND))
+    }
 
 }
