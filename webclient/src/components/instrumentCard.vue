6937e663b85b2342e04f26e200d4961380e0d4a4
Fatih <fatihy@yahoo.com>
2021-01-10 02:47:33 +0300

    Enstruman kartı eklendi. Ana menü toolbar'ı eklendi.

---
+++ webclient/src/components/instrumentCard.vue
@@ -0,0 +1,48 @@
+<template>
+  <v-card
+  color="primary"
+   class="mx-auto my-5 d-flex justify-center"
+   height="325"
+   min-width="500">
+    <v-container>
+        <v-row>
+            <v-col>
+                <v-img
+                height="300"
+                width="400"
+                src="https://via.placeholder.com/300">
+                </v-img>
+            </v-col>
+
+            <v-col>
+                <v-card-title>{{ instrument.name }}</v-card-title>
+                <p> {{ instrument.info }} </p>
+            </v-col>
+
+        </v-row>
+    </v-container>
+
+  </v-card>
+</template>
+
+<script>
+export default {
+  name: 'instrumentCard',
+  data: () => ({
+    instrument: {
+      renter_public_uid: '',
+      photos: [],
+      name: 'Fender C100S',
+      info: 'Test ediliyor',
+      category: '',
+      shop_name: '',
+      open_to_sell: null,
+      price: null
+    }
+  })
+}
+</script>
+
+<style>
+
+</style>
--- webclient/src/views/Home.vue
+++ webclient/src/views/Home.vue
@@ -1,15 +1,44 @@
 <template>
-      <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <v-container>
+    <v-toolbar class="mt-0 mx-0" color="primary" elevation="10">
+        <!-- Buttons -->
+        <v-btn text v-for="(button, index) in toolbar_buttons" :key="index"
+          @click="handleFunctionCall(button.target_function)">
+          {{ button.text }}
+        </v-btn>
+        <!-- /Buttons -->
+
+        <v-spacer> </v-spacer>
+        <!-- Possible Search Bar position -->
+    </v-toolbar>
+    <instrumentCard />
+  </v-container>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import instrumentCard from '@/components/instrumentCard.vue'
 
 export default {
   name: 'Home',
+  data: () => ({
+    toolbar_buttons: [
+      { text: 'Test', target_function: 'test' },
+      { text: 'Test', target_function: 'test' },
+      { text: 'Test', target_function: 'test' }
+    ]
+  }),
   components: {
-    HelloWorld
+    instrumentCard
+  },
+  methods: {
+    // Redirector function.
+    handleFunctionCall (func) {
+      if (func === 'test') this.test()
+    },
+    test () {
+      alert('Works!')
+    }
   }
 }
 </script>
